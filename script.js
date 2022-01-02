3eadf571d7907566d0ce6e2f56a53ca47250cc1c
Naheed Tayab <naheedtayab18@gmail.com>
2022-01-02 15:19:24 +0000

    Site fully functional

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-async function getRandomFacts() {
-    const randomFact = fetch("https://uselessfacts.jsph.pl/random.json?language=en");
+getRandomFact();
+
+const fact = document.getElementById('fact');
+const fact_btn = document.getElementById('fact_btn');
+fact_btn.addEventListener('click', getRandomFact)
+
+function getRandomFact() {
+    fetch("https://uselessfacts.jsph.pl/random.json?language=en")
+        .then(resp => resp.json())
+        .then(data => fact.innerHTML = `<p>${data.text}</p>`);
 }
